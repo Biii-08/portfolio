4538a5be8c18e22968f47e2ea455e3ed1de80c45
biii-08 <bibishaguragai656@gmail.com>
2023-06-06 16:02:35 -0700

    second commit

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,8 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import {AiOutlineclose,AiOutlineMenu} from 'react-icons/ai';
+import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai';
+import {FaLinkedinIn} from 'react-icons/fa';
 
 const Nav = () => {
   return (
@@ -27,8 +28,8 @@ const Nav = () => {
                     </Link>
 
                 </ul>
-                <div className='md:hidden'>
-                    <AiOutlineMenu size={25}/>
+                  <div className='md:hidden'>
+                        <AiOutlineMenu size={25}/>
                 </div>
             </div>
             </div>
@@ -37,11 +38,37 @@ const Nav = () => {
             <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
                 <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-{45%] h-screen bg-white p-10 ease-in duration-500'>
                  <div>
-                 <div className='rounded-full shadow-large shadow-gray-400 p-3 cursor-pointer'>
-                    <AiOutlineclose/>
+                    <div className='flex w-full items-center justify-between'>
+                        <image src="/images.png" alt="/" width='87' height='35'/>      
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                    <AiOutlineClose/>
                     
-                 </div> 
-                 </div> 
+                        </div> 
+                    </div>
+                 </div>
+
+                    <div className='border-b border-gray-300 my-4'>
+                    <p className='w-[85%] md:w-[90%] py-4'> Building a website using next  </p>
+                    </div>  
+                </div>
+                <div className='py-4 flex flex-col'>
+                    <ul className='uppercase'>
+                        <Link href='/'>
+                            <li className='py-4 text-sm' > Home </li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='py-4 text-sm' > About </li>
+                        </Link> 
+                        <Link href='/'>
+                            <li className='py-4 text-sm' > Contact </li>
+                        </Link>
+                    </ul>
+                    <div className='pt-40'>
+                        <p className='uppercase tracking-widest text-[#5651e5]'> Lets connect</p>
+                        <div>
+                            <FaLinkedinIn/>
+                        </div>
+                    </div>
                 </div>
             </div>
            </div>
